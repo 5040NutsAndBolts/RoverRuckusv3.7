0201bbb71ba91a5c895a8bd10a5e97ab4bccc8b9
Spitz <robotics5040@gmail.com>
2018-09-19 16:28:07 -0400

    I reprogrammed the mecanum wheels to work, need to test this on Sunday

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveTrainChocks.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveTrainChocks.java
@@ -1,23 +1,36 @@
 package org.firstinspires.ftc.teamcode;
 
-public class driveTrainChocks
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.Range;
+
+public class driveTrainChocks extends OpMode
 {
-private DcMotor topLeftDrive = null;
-private DcMotor topRightDrive = null;
-private DcMotor bottomLeftDrive = null;
-private DcMotor bottomRightDrive = null;
+private DcMotor leftFront = null;
+private DcMotor rightFront = null;
+private DcMotor leftRear = null;
+private DcMotor rightRear = null;
 
-    public void runOpMode()
-    {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+    public void init() {
 
-        topLeftDrive = hardwareMap.get(DcMotor.class, "topLeftDrive");
-        topRightDrive = hardwareMap.get(DcMotor.class, "topRightDrive");
-        bottomLeftDrive = hardwareMap.get(DcMotor.class, "bottomLeftDrive");
-        bottomRightDrive = hardwareMap.get(DcMotor.class; "bottomRightDrive")
-
-        topLeftDrive.setDirection(DcMotor.)
-        topLeftDrive.setDirection(DcMotor.Direction.)
     }
+
+    public void loop()
+    {
+        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double rightX = gamepad1.right_stick_x;
+        final double v1 = r * Math.cos(robotAngle) + rightX;
+        final double v2 = r * Math.sin(robotAngle) - rightX;
+        final double v3 = r * Math.sin(robotAngle) + rightX;
+        final double v4 = r * Math.cos(robotAngle) - rightX;
+
+        leftFront.setPower(v1);
+        rightFront.setPower(v2);
+        leftRear.setPower(v3);
+        rightRear.setPower(v4);
+    }
+
+
 }
